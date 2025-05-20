48510ede7bcfb4380ff9306ed851578bc575ec3f
Ammar <ammarasafi041@gmail.com>
2025-05-20 23:27:00 +0300

    php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$ex = 0;
+$res = 0;
+$data = "C:\Users\HP\Pictures\Screenshots\Screenshot(319).png a4ldsfm31opsd8f9039jfp3jf";
+// header("Content-Type: application/json");
+
+header("Content-Type:application/json");
+exec("py blockchain.py " . $data , $ex , $re);
+
+// echo "<pre>";
+return $ex;
